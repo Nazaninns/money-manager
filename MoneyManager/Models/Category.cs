57b816d2580db41db98f57c07cff47eaf23be723
nazaninns <nazaninnsq@gmail.com>
2026-07-08 22:22:18 +0330

    Apply .gitignore

---
+++ MoneyManager/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyManager.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    //Navigation property
+    public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
+}
---
+++ MoneyManager/Models/Expense.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyManager.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public int CategoryId { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    //Navigation property
+    public Category Category { get; set; } = null!;
+}
