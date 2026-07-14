53cbf36479bf890ac42be516345d8ccc2eb8c418
nazaninns <nazaninnsq@gmail.com>
2026-07-14 18:19:52 +0330

    add Expense service and repository to dependency injection

--- MoneyManager/Program.cs
+++ MoneyManager/Program.cs
@@ -16,7 +16,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 //controller
 builder.Services.AddControllers()
-    .AddJsonOptions(options => options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)
     .ConfigureApiBehaviorOptions(options =>
         {
             options.InvalidModelStateResponseFactory = context =>
@@ -29,16 +30,19 @@ builder.Services.AddControllers()
                 return new BadRequestObjectResult(customRes);
             };
         }
-        )
+    )
     ;
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 //DI
-builder.Services.AddDbContext<AppDbContext>(options => 
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+builder.Services.AddScoped<IExpenseService, ExpenseService>();
+builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
 
 var app = builder.Build();
 
